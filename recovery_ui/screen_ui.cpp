8d80eef87d32a873f904df71edf929638247f0e6
Gor Mirzoyan <37484454+xwtk@users.noreply.github.com>
2025-09-17 21:27:51 +0400

    Update colors

--- recovery_ui/screen_ui.cpp
+++ recovery_ui/screen_ui.cpp
@@ -565,23 +565,23 @@ void ScreenRecoveryUI::SetColor(UIElement e) const {
       if (fastbootd_logo_enabled_)
         gr_color(0xfd, 0xd8, 0x35, 255);
       else
-        gr_color(0xf8, 0x90, 0xff, 255);
+        gr_color(0xc8, 0xc8, 0xc8, 255);
       break;
     case UIElement::HEADER:
       if (fastbootd_logo_enabled_)
         gr_color(0xfd, 0xd8,0x35, 255);
       else
-        gr_color(0xf8, 0x90, 0xff, 255);
+        gr_color(0xc8, 0xc8, 0xc8, 255);
       break;
     case UIElement::MENU:
-      gr_color(0xd8, 0xd8, 0xd8, 255);
+      gr_color(0xa8, 0xa8, 0xa8, 255);
       break;
     case UIElement::MENU_SEL_BG:
     case UIElement::SCROLLBAR:
       if (fastbootd_logo_enabled_)
         gr_color(0xe6, 0x51, 0x00, 255);
       else
-        gr_color(0x7c, 0x4d, 0xff, 255);
+        gr_color(0xa8, 0xa8, 0xa8, 255);
       break;
     case UIElement::MENU_SEL_BG_ACTIVE:
       gr_color(0, 156, 100, 255);
